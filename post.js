e7811eb36ed2714ebf4e625628f74545b61f7373
apizzimenti <apizzime@gmu.edu>
2023-12-12 19:48:43 -0500

    blog infrastructure

--- post.js
+++ post.js
@@ -1,14 +1,27 @@
 
+var converter = new showdown.Converter();
+
 $(document).ready(function(){
     // Load the right post when the head of the page loads.
     url = new URL(window.location.href);
     path = url.searchParams.get("path").toString();
+    title = path.split("/").slice(-1);
 
-    // Load the correct post and change the page title.
-    $("#post").load(
-        path + ".html",
-        function () {
-            document.title = path.split("/").slice(-1);
-        }
-    );
+    // Read the markdown text and convert.
+    $.get(path+".md", function(markdown){
+        html = converter.makeHtml(markdown);
+        html = "<h2 class='blog-title'>" + title + "</h2>" + html;
+
+        // Load the correct post and change the page title.
+        $("#post").html(html);
+        MathJax.typeset();
+        hljs.highlightAll();
+        document.title = title;
+    });
 });
+
+function attemptMathJax() {
+    while(typeof MathJax === undefined) {
+        
+    };
+}
